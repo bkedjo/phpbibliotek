07092481b2111c84f04ed408c2e68af35614e22a
bkedjo <dpakson23@gmail.com>
2024-03-15 08:07:54 -0400

    implementation du fichier index

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-    echo "je suis dans index";
+
+define("ROOT", str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']));
+define("URI", 'http://localhost/bibliophp/');
+require_once ROOT . "autoload.php";
+$params = explode("/", $_GET['p']); //    auths/register/
+// print_r($params);
+if ($params[0] != "") {
+
+    $nomController = ucfirst($params[0]);//    $params[0] = Auths $params[1] =register
+    if (file_exists(ROOT . "controllers/" . $nomController . ".php")) { // bibliophp/controllers/Auths.php
+        $controller = new $nomController(); //  $oAuths = new Auths();
+        $action = $params[1];
+       
+        $controller->$action(); // $controller->inscription();
+     
+    } else {
+        echo "Controller don't exist";
+    }
+
+} else {
+    echo "Add param controller";
+}
 ?>
